1569f2f117a2e7d6b8182c0fc3200e9ab9213856
Rody Huancas <rodyhuancas.04@gmail.com>
2024-06-30 15:06:09 -0500

    Add: search user

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,29 +1,37 @@
+import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
 
-export const Search = () => {
-	return (
-		<form className='max-w-xl mx-auto p-2 sm:w-96'>
-			<label htmlFor='default-search' className='mb-2 text-sm font-medium text-gray-900 sr-only'>
-				Search
-			</label>
-			<div className='relative'>
-				<div className='absolute inset-y-0 start-0 flex items-center z-10 ps-3 pointer-events-none'>
-					<IoSearch className='w-5 h-5' />
-				</div>
-				<input
-					type='search'
-					id='default-search'
-					className='block w-full p-4 ps-10 text-sm rounded-lg bg-transparent focus:bg-transparent border border-gray-100/90'
-					placeholder='Ejm: rody-huancas'
-					required
-				/>
-				<button
-					type='submit'
-					className='absolute end-2.5 bottom-1.5 text-gray-900/90 bg-white py-2 px-5 rounded-lg'
-				>
-					Buscar
-				</button>
-			</div>
-		</form>
-	);
+export const Search = ({ onSearch }) => {
+  const [username, setUsername] = useState("");
+
+  return (
+    <form className="max-w-xl mx-auto p-2" onSubmit={(e) => onSearch(e, username)}>
+      <label
+        htmlFor="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only"
+      >
+        Search
+      </label>
+      <div className="relative">
+        <div className="absolute inset-y-0 start-0 flex items-center z-10 ps-3 pointer-events-none">
+          <IoSearch className="w-5 h-5" />
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          className="block w-full p-4 ps-10 text-sm rounded-lg bg-transparent focus:bg-transparent border border-gray-100/90"
+          placeholder="Ejm: rody-huancas"
+          required
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="absolute end-2.5 bottom-1.5 text-gray-900/90 bg-white py-2 px-5 rounded-lg"
+        >
+          Buscar
+        </button>
+      </div>
+    </form>
+  );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,37 +10,56 @@ export const HomePage = () => {
   const [loading, setloading] = useState(false);
   const [sortType, setSortType] = useState("forks");
 
-  const getUserProfileAndRepos = useCallback(async () => {
-    setloading(true);
-    try {
-      const userRes = await fetch("https://api.github.com/users/rody-huancas");
-      const userProfile = await userRes.json();
-      setUserProfile(userProfile);
+  const getUserProfileAndRepos = useCallback(
+    async (username = "rody-huancas") => {
+      setloading(true);
+      try {
+        const userRes = await fetch(`https://api.github.com/users/${username}`);
+        const userProfile = await userRes.json();
+        setUserProfile(userProfile);
 
-      const repoRes = await fetch(userProfile.repos_url);
-      const repos = await repoRes.json();
-      setRepos(repos);
-    } catch (error) {
-      toast.error(error.message);
-    } finally {
-      setloading(false);
-    }
-  }, []);
+        const repoRes = await fetch(userProfile.repos_url);
+        const repos = await repoRes.json();
+        setRepos(repos);
+
+        return { userProfile, repos };
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setloading(false);
+      }
+    },
+    []
+  );
 
   useEffect(() => {
     getUserProfileAndRepos();
   }, [getUserProfileAndRepos]);
 
+  const onSearch = async (e, username) => {
+    e.preventDefault();
+
+    setloading(true);
+    setRepos([]);
+    setUserProfile(null);
+
+    const { userProfile, repos } = await getUserProfileAndRepos(username);
+
+    setUserProfile(userProfile);
+    setRepos(repos);
+    setloading(false);
+  };
+
   return (
     <div className="m-4">
-      <Search />
+      <Search onSearch={onSearch} />
       <SortRepos />
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
-        { userProfile && <ProfileInfo userProfile={userProfile} /> }
-        
-        { repos.length > 0 && !loading && <Repos repos={repos} /> }
+        {userProfile && <ProfileInfo userProfile={userProfile} />}
 
-        { loading && <Spinner /> }
+        {repos.length > 0 && !loading && <Repos repos={repos} />}
+
+        {loading && <Spinner />}
       </div>
     </div>
   );
